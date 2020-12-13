d55c3a614a4b976e2a494d494fd4f77b8bda38f5
nyan-left <deim.jak@gmail.com>
2020-12-13 22:47:24 +0000

    add simple cors proxy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export { default as ITEMS_LIST } from './data/items';
 export { default as getTradeVolume } from './API/tradeVolume';
 export { default as getFromOfficialAPI } from './API/official';
 export { default as getFrom2007HQ } from './API/2007hq';
+export let PROXY = '';
