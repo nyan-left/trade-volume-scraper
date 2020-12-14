1b7d561a9ca8e09a4c8418f6ebe36aead0bd9aae
nyan-left <deim.jak@gmail.com>
2020-12-14 05:36:55 +0000

    Finish feature

--- src/API/tradeVolume.ts
+++ src/API/tradeVolume.ts
@@ -1,14 +1,14 @@
-import { PROXY } from './../index';
 import axios from 'axios';
 import * as _ from 'lodash';
 import * as moment from 'moment';
 import * as UserAgent from 'user-agents';
 import { getFromOfficialAPI } from '../index';
 import { Day, FullItemData } from '../spec';
+import proxyURL from './proxy';
 
 function getItemUrl(ID: number, item_name: string): string {
   item_name = item_name.replace(' ', '+');
-  return `${PROXY}http://services.runescape.com/m=itemdb_oldschool/${item_name}/viewitem?obj=${ID}`;
+  return `${proxyURL}http://services.runescape.com/m=itemdb_oldschool/${item_name}/viewitem?obj=${ID}`;
 }
 
 const parseHTML = (html: string): Day[] => {
